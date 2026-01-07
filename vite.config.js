811d41754ecc721011209a9e44c14f1e35ffcb5d
de2425c <154690407+de2425c@users.noreply.github.com>
2026-01-07 15:09:19 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        support: resolve(__dirname, 'support.html'),
+        '404': resolve(__dirname, '404.html'),
+      },
+    },
   },
 })
